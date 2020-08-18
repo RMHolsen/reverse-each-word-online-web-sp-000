522c9c8817cbb7d417d86ee510637902b5de61c4
Rosario Holsen <rholsen@gmail.com>
2020-08-18 15:37:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,7 @@
 def reverse_each_word(sentence)
   array = sentence.split
-  array.collect {|word| word.reverse}
-  array.join(" ")
+  new_array = []
+  array.collect {|word| new_array << word.reverse}
+  new_array
+  new_array.join(" ")
 end
